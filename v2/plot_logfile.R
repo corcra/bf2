da0b1925847e8a96a61ba44eb06d37470c949af4
stephanie <sh985@cornell.edu>
2015-03-03 14:33:25 -0500

    extremely simple script for plotting the energies coming out of a logfile

---
+++ v2/plot_logfile.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+library(reshape2)
+
+args<-commandArgs(TRUE)
+fname<-args[1]
+da<-read.table(fname,header=TRUE)
+energies<-da[,c("n","de","me","ve","re")]
+
+el<-melt(energies,id="n")
+ggplot(el, aes(x=n, y=value, color=variable))+geom_point()+xlab("# training examples seen")+ylab("mean energy of batch")+ggtitle(fname)
+ggsave(paste0(fname,"_energies.pdf"))
