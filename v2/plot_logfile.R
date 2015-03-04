4d00155d2d0f07bb2685585498a7f98da9234fc1
stephanie <sh985@cornell.edu>
2015-03-04 12:09:02 -0500

    also plots log-likelihood, but doesn't check to see if it can, so be warned

--- v2/plot_logfile.R
+++ v2/plot_logfile.R
@@ -5,7 +5,11 @@ args<-commandArgs(TRUE)
 fname<-args[1]
 da<-read.table(fname,header=TRUE)
 energies<-da[,c("n","de","me","ve","re")]
+dll<-da[,c("n","ll")]
 
 el<-melt(energies,id="n")
 ggplot(el, aes(x=n, y=value, color=variable))+geom_point()+xlab("# training examples seen")+ylab("mean energy of batch")+ggtitle(fname)
 ggsave(paste0(fname,"_energies.pdf"))
+
+ggplot(dll, aes(x=n, y=ll))+geom_point()
+ggsave(paste0(fname,"_ll.pdf"))
