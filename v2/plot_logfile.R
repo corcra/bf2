77bdf7cc81f9773e9fd79fc78b4eb82103166b43
stephanie <sh985@cornell.edu>
2015-03-06 15:10:35 -0500

    nicer colours :)

--- v2/plot_logfile.R
+++ v2/plot_logfile.R
@@ -4,11 +4,17 @@ library(reshape2)
 args<-commandArgs(TRUE)
 fname<-args[1]
 da<-read.table(fname,header=TRUE)
-energies<-da[,c("n","de","me","ve","re")]
+energies<-da[,c("n","data_energy","model_energy","valiset_energy","random_energy")]
+#energies<-da[,c("n","de","me","ve","re")]
 dll<-da[,c("n","ll")]
 
+datac<-"deepskyblue"
+modelc<-"springgreen3"
+valic<-"black"
+randomc<-"gray50"
+
 el<-melt(energies,id="n")
-ggplot(el, aes(x=n, y=value, color=variable))+geom_point()+xlab("# training examples seen")+ylab("mean energy of batch")+ggtitle(fname)
+ggplot(el, aes(x=n, y=value, color=variable))+geom_point(cex=1.2, alpha=0.8)+xlab("# training examples seen")+ylab("mean energy of batch")+ggtitle(fname)+scale_color_manual(values=c(datac, modelc, valic, randomc))
 ggsave(paste0(fname,"_energies.pdf"))
 
 ggplot(dll, aes(x=n, y=ll))+geom_point()
