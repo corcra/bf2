5c5cf7f8a47378e8c10e9022b70c5f18697cf250
Stephanie Hyland <steph.hyland@gmail.com>
2015-06-28 12:22:09 -0400

    moved

---
+++ plot_logfile.R
@@ -0,0 +1,55 @@
+library(ggplot2)
+library(reshape2)
+
+args<-commandArgs(TRUE)
+fname_raw<-args[1]
+da<-read.table(fname_raw,header=TRUE, na.strings=c("nan"))
+fname<-gsub("_logfile.txt", "", fname_raw)
+da<-na.omit(da)
+
+datac<-"deepskyblue"
+modelc<-"springgreen3"
+valic<-"black"
+randomc<-"gray50"
+permc<-"tomato1"
+
+# energies
+if (da$model_energy[1] == "NA"){
+    energies<-da[,c("n","data_energy","valiset_energy","random_energy", "perm_energy")]
+    colz<-c(datac, valic, randomc, permc)
+} else{
+    energies<-da[,c("n","data_energy","model_energy","valiset_energy","random_energy", "perm_energy")]
+    colz<-c(datac, modelc, valic, randomc, permc)
+}
+el<-melt(energies,id="n")
+ggplot(el, aes(x=n, y=value, color=variable))+geom_point(cex=1.2, alpha=0.8)+xlab("# training examples seen")+ylab("mean energy of batch")+ggtitle(fname)+scale_color_manual(values=colz)
+#ggsave(paste0(fname,"_energies.pdf"))
+ggsave(paste0(fname,"_energies.png"))
+
+# log likelihood
+dll<-da[,c("n","ll")]
+if (sd(dll$ll) > 0){
+    ggplot(dll, aes(x=n, y=ll))+geom_point()+ggtitle(fname)
+    #ggsave(paste0(fname,"_ll.pdf"))
+    ggsave(paste0(fname,"_ll.png"))
+} else{
+    print("not plotting log-likelihood (variance 0, probably not recorded)")
+}
+
+# compare ll with weirdratio
+#ratio<-abs(da$valiset_energy/da$perm_energy-(da$valiset_energy/da$perm_energy)[1])
+#ratio<-ratio/max(ratio)
+#lln<-abs(da$ll-da$ll[1])
+#lln<-lln/max(lln)
+#dlr<-data.frame(da$n, ratio, lln)
+#names(dlr)<-c("n","ratio","abs(ll)")
+#dlrl<-melt(dlr, id="n")
+#ggplot(dlrl, aes(x=n, y=value, color=variable))+geom_point()+xlab("# training examples seen")+ylab("rescaled measure")+ggtitle(fname)
+#ggsave(paste0(fname,"_rtest.pdf"))
+
+# look at vector lengths innit
+lens<-da[,c("n", "C_lens", "G_lens", "V_lens")]
+lensl<-melt(lens, id="n")
+ggplot(lensl, aes(x=n, y=value, color=variable))+geom_point(cex=1.2, alpha=0.8)+xlab("# training examples seen")+ylab("mean length of sample of vectors/matrices")+ggtitle(fname)
+#ggsave(paste0(fname,"_lens.pdf"))
+ggsave(paste0(fname,"_lens.png"))
