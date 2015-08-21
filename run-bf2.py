679b5049a939ba8ec3e4ebfc5a334462e656ae42
Stephanie Hyland <steph.hyland@gmail.com>
2015-08-21 18:10:46 -0400

    tidied up a lot of things relating to paths, loading the options

--- run-bf2.py
+++ run-bf2.py
@@ -6,18 +6,9 @@ import cProfile
 import re
 from subprocess import call
 import sys
+import os.path
 
-#DATA='sem_net'
-#DATA='w8r3'
-#DATA='w5r1'
-#DATA='nips13_wordnet'
-#DATA='nips13_wordnet_simple'
-#DATA='nips13_freebase'
-#DATA='direct_msk_small'
-#DATA = 'wiki_subsamp0.0005'
 DATA = 'cfg'
-
-print DATA
 
 #ONLINE=True
 ONLINE=False
@@ -31,6 +22,10 @@ if EXACT or NOISE: PERSISTENT=False
 #CALC_LL=True
 CALC_LL = False
 #CALC_LL=not(ONLINE)
+
+# --- paths --- #
+outroot = './output'
+droot = './data'
 
 # --- encode options in filename --- #
 if ONLINE:
@@ -66,46 +61,22 @@ etype = bf2f.ETYPE
  
 # --- training options --- #
 if len(sys.argv) > 1:
-    # assume an options-file path has been provided
-    # BRITTLE 
-    options_in_path = sys.argv[1]
-    print 'Reading options from',  options_in_path
-    options_raw = open(options_in_path, 'r').readlines()
-    options = dict()
-    for line in options_raw:
-        if '#' in line:
-            # skip 'comments'
-            continue
-        option_name = line.split(' ')[0]
-        option_value = ' '.join(line.split(' ')[1:])
-        # this is gross
-        if '(' in option_value:
-            value = tuple(map(float, re.sub('[\(\)]', '', option_value).split(',')))
-        elif '[' in option_value:
-            value = bf2f.np.array(map(float, re.sub('[\[\]]', '', option_value).split(',')))
-        elif option_value == 'False\n':
-            value = False
-        elif option_value == 'True\n':
-            value = True
-        else:
-            try:
-                value = int(option_value)
-            except ValueError:
-                # not an int
-                value = option_value.strip()
-        options[option_name] = value
+    options = bf2f.load_options(sys.argv[1])
     d = options['dimension']
     D = options['diagnostics_rate']
     fix_words = options['fix_words']
     fix_relas = options['fix_relas']
     trans_rela = options['trans_rela']
     n_epochs = options['n_epochs']
-    options['mu'] = bf2f.np.array(options['mu'])
-    options['nu'] = bf2f.np.array(options['nu'])
-    options['alpha'] = bf2f.np.array(options['alpha'])
-    options['omega'] = bf2f.np.array(options['omega'])
     CALC_LL = options['calculate_ll']
+    # optional options :)
+    if 'wordlist' in options:
+        wordlist = options['wordlist']
+    if 'relalist' in options:
+        wordlist = options['wordlist']
 else:
+    print 'No options file provided, falling back to defaults.'
+    # default options
     B = 100
     S = 100
     M = 5
@@ -139,9 +110,10 @@ else:
                'trans_rela':trans_rela,
                'n_epochs':n_epochs,
                'offset':offset}
+    for (key, value) in options.iteritems():
+        print value, '\t:', key
     # note that some of these options are not used by bf2f
 
-outpath = '/cbio/grlab/home/hyland/git/bri-focal/v2/output/'
 if 'name' in options:
     # THIS IS DANGEROUS
     fname = options['name']
@@ -158,11 +130,28 @@ if 'name' in options:
     if 'SGD' in fname:
         assert bf2f.SGD
 else:
-    fname = outpath+DATA+'_'+online_or_batch+'_'+exact_or_not+'_'+persistent_or_not+'_'+str(d)+'d_'+train_method+'_'+normed+'_'+etype
+    fname = outroot+'/'+DATA+'_'+online_or_batch+'_'+exact_or_not+'_'+persistent_or_not+'_'+str(d)+'d_'+train_method+'_'+normed+'_'+etype
     options['name'] = fname
 paramfile = fname+'_XXX.txt'
 valifile = fname+'_valiset.txt'
 optionsfile = fname+'_options.txt'
+   
+# --- datafiles --- #
+
+# overwrite from the options file
+# WARNING: DANGEROUS
+if 'dpath' in options:
+    dpath = options['dpath']
+else:
+    print 'No training data provided, using toy data.'
+    W, R = 1000, 5
+    dpath = droot+'/w'+str(W)+'r'+str(R)+'_train.txt.gz'
+    if not os.path.isfile(dpath):
+        # create data if it doesn't exist
+        bf2f.generate_traindata(droot, W, R)
+        assert os.path.isfile(dpath)
+    options['dpath'] = dpath
+
 # save the options
 fo = open(optionsfile,'w')
 for (option, value) in options.iteritems():
@@ -170,68 +159,6 @@ for (option, value) in options.iteritems():
         value = tuple(value)
     fo.write(option+' '+str(value)+'\n')
 fo.close()
-   
-# --- datafiles --- #
-droot = ''
-
-#(for regression testing)
-#dpath = droot+w5_r2_train.txt.gz'
-#dpath = droot+'w8r3_train.txt.gz'
-#dpath = droot+'w1000r5_train.txt.gz'
-#dpath = droot+'w100r5_train.txt.gz'
-#dpath = 'sampled_w5_train.txt.gz'
-#dpath = 'w100_train.txt.gz'
-#dpath = 'w100r5_train.txt.gz'
-#dpath = 'w100r5b_train.txt.gz'
-#dpath = 'TRAINDATA_toy_100_R1_w2v_dotv2.txt.gz'
-#dpath = 'w100r4_train.txt.gz'
-#dpath = 'sampled_w5_train.txt.gz'
-#dpath = droot+'w1000r1_train.txt.gz'
-
-
-if DATA == 'sem_net':
-    # umls semantic network stuff
-    # (5x the training data)
-    dpath = ''
-    wordlist = ''
-    relasfile = ''
-elif DATA == 'wiki_subsamp0.0005':
-    # wikipedia subsample
-    dpath = ''
-    wordlist =  ''
-elif DATA == 'w5r1':
-    dpath = droot+'w5_train.txt.gz'
-elif DATA == 'w8r3':
-    dpath = droot+'w8r3_train.txt.gz'
-elif DATA == 'nips13_wordnet':
-    #wordnet examples from socher + chen, nips 2013
-    dpath = ''
-    wordlist = '/'
-    relasfile = ''
-elif DATA == 'nips13_wordnet_simple':
-    #wordnet examples from socher + chen, nips 2013
-    dpath = ''
-    wordlist = ''
-    relasfile = ''
-elif DATA == 'nips13_freebase':
-    #freebase examples from socher + chen, nips 2013
-    dpath = ''
-    wordlist = ''
-    relasfile = ''
-elif DATA == 'direct_msk_small':
-    dpath = ''
-    wordlist = ''
-elif not len(sys.argv) > 1:
-        sys.exit('ERROR: no data!')
-else:
-    print 'Expecting dpath in config file.'
-
-# overwrite from the options file
-# WARNING: DANGEROUS
-if 'dpath' in options:
-    dpath = options['dpath']
-if 'wordlist' in options:
-    wordlist = options['wordlist']
 
 # --- initialise things --- #
 dstream = bf2f.data_stream(dpath)
@@ -274,7 +201,7 @@ if D > 0:
 else:
     DIAGNOSTICS = False
 
-
+# --- get vocabulary (words, relationships) --- #
 try:
     words_raw = open(wordlist, 'r').readlines()
     print 'Reading vocabulary from', wordlist
@@ -284,17 +211,19 @@ try:
         word = line.split()[1]
         words[index] = word
 except NameError:
+    print 'WARNING: wordlist doesn\'t exist.'
     # wordlist doesn't exist
     words = map(str, range(W))
 try:
-    relas_raw = open(relasfile, 'r').readlines()
+    relas_raw = open(relalist, 'r').readlines()
     relas = ['']*len(relas_raw)
     for line in relas_raw:
         index = int(line.split()[0])
         rela = line.split()[1]
         relas[index] = rela
 except NameError:
-    # relasfile doesn't exist
+    print 'WARNING: relalist doesn\'t exist.'
+    # relalist doesn't exist
     relas = map(str, range(R))
 
 vocab = {'words':words, 'relas': relas}
