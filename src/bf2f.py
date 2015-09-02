27758188c08834a706c6a514133e8fab9607be26
Stephanie Hyland <steph.hyland@gmail.com>
2015-09-02 11:49:25 -0400

    making cfg files less confusing: what was 'name' is now 'output_root', and what was 'dpath' is now 'training_data_path' (although that doesn't appear in this particular script)

--- src/bf2f.py
+++ src/bf2f.py
@@ -80,10 +80,12 @@ def load_options(options_path):
                 value = option_value.strip()
         options[option_name] = value
     # make np arrays
-    options['mu'] = bf2f.np.array(options['mu'])
-    options['nu'] = bf2f.np.array(options['nu'])
-    options['alpha'] = bf2f.np.array(options['alpha'])
-    options['omega'] = bf2f.np.array(options['omega'])
+    options['mu'] = np.array(options['mu'])
+    options['nu'] = np.array(options['nu'])
+    options['alpha'] = np.array(options['alpha'])
+    # optional
+    if 'omega' in options:
+        options['omega'] = np.array(options['omega'])
     return options
 
 def generate_traindata(droot, W, R):
@@ -807,8 +809,8 @@ def train(training_data, start_parameters, options,
     mu_t = mu[:]
     alpha = alpha0[:]
     tau = options['tau']
-    name = options['name']
-    print name
+    output_root = options['output_root']
+    print output_root
     offset = options['offset']
     try:
         vali_set_size = options['vali_set_size']
@@ -825,7 +827,7 @@ def train(training_data, start_parameters, options,
     else:
         parameters = start_parameters
     # diagnostic things
-    logf = open(name+'_logfile.txt','a')
+    logf = open(output_root+'_logfile.txt','a')
     W = parameters.W
     R = parameters.R
     try:
@@ -942,12 +944,12 @@ def train(training_data, start_parameters, options,
                 #devset_accuracy(devpath, devlogpath, parameters, n + offset)
                 # endyolo
             if n%(D*10) == 0:
-                parameters.save(name+'_XXX.npy')
+                parameters.save(output_root+'_XXX.npy')
                 if VERBOSE:
-                    print 'Saved parameters to', name+'_XXX.npy'
+                    print 'Saved parameters to', output_root+'_XXX.npy'
     logf.close()
     if VERBOSE: print 'Training done,', n, 'examples seen.'
-    parameters.save(name+'_XXX.npy')
+    parameters.save(output_root+'_XXX.npy')
     options['alpha'] = alpha
     options['offset'] += n
     return vali_set
